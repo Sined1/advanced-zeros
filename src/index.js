4fa1a536a1217eecac1eb438664a0ad179b3d771
Denis Babuskin <3169477@gmail.com>
2018-10-01 14:33:29 +0300

    add function of recognizing if the value is prime

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  function isPrime(value) {
+    let ar = [true];
+    for (let i = 2; i <= value; i++) {
+      if (value % i == 0) {
+        ar.push(true);
+      }
+    }
+    if (ar.length == 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  
 }
